b3ac8829a752ffddf79f72b19d7d5c7a93fbdd1d
thief01 <pst12345@wp.pl>
2023-02-04 21:15:38 +0100

    add fader

---
+++ Scripts/Tests/TestFade.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            FadeScreen.Instance.FadeIn();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            FadeScreen.Instance.FadeOut();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            FadeScreen.Instance.FadeInOut(1, 2);
+        }
+    }
+}

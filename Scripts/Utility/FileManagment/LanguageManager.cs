a0319e5efdedc0cd114157bba712566043d1c4e4
thief01 <pst12345@wp.pl>
2023-05-21 01:22:06 +0200

    language window editor - sketch

--- Scripts/Utility/FileManagment/LanguageManager.cs
+++ Scripts/Utility/FileManagment/LanguageManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Xml;
 using DependentObjects.ScriptableObjects;
 using DependentObjects.ScriptableObjects.Profiles;
@@ -9,12 +10,19 @@ namespace Utility.FileManagment
 {
     public class LanguageManager
     {
+        public static string LANG_PATH
+        {
+            get
+            {
+                return Application.dataPath + "/Resources/Configs/Langs/";
+            }
+        }
         public static UnityEvent LanguageChanged = new UnityEvent();
         
         private static Dictionary<string, string> LoadedLang;
-        public static void LoadLang()
+        public static void LoadLanguage()
         {
-            var path = Application.dataPath + "/Resources/Configs/Langs/" + ApplicationProfile.Instance.Language + ".xml";
+            var path = LANG_PATH + ApplicationProfile.Instance.Language + ".xml";
         
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
@@ -28,11 +36,38 @@ namespace Utility.FileManagment
             LoadedLang = d;
             LanguageChanged.Invoke();
         }
+        
+        public static Dictionary<string, string> GetLanguage(string lang)
+        {
+            var path = LANG_PATH + lang + ".xml";
+        
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+        
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            foreach (XmlNode n in doc.SelectNodes("/data/*"))
+            {
+                d.Add(n.Name, n.InnerText);
+            }
 
-        public static string GetTransation(string keyWord)
+            return d;
+        }
+
+        public static string[] GetLanguagesList()
+        {
+            var langs =Directory.GetFiles(LANG_PATH, "*.xml");
+            for (int i = 0; i < langs.Length; i++)
+            {
+                langs[i] = Path.GetFileName(langs[i]);
+            }
+
+            return langs;
+        }
+
+        public static string GetTranslation(string keyWord)
         {
             if(LoadedLang==null)
-                LoadLang();
+                LoadLanguage();
             return LoadedLang[keyWord];
         }
     }

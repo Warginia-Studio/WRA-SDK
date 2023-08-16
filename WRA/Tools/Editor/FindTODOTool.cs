2c1ab3ed8bff540a27377a0d3874d1d685a67f8f
thief01 <pst12345@wp.pl>
2023-08-16 08:35:25 +0200

    add namespaces

--- WRA/Tools/Editor/FindTODOTool.cs
+++ WRA/Tools/Editor/FindTODOTool.cs
@@ -1,53 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using WRA.Utility.Diagnostics;
 
-public class FindTODOTool : MonoBehaviour
-{ 
-    [MenuItem("thief01/Tools/Find TODO's comments")]
-    public static void Find()
-    {
-        ProcessDirectory(Application.dataPath);
-    }
-    
-    private static void ProcessDirectory(string targetDirectory)
-    {
-        // Process the list of files found in the directory.
-        string [] fileEntries = Directory.GetFiles(targetDirectory, "*.cs");
-        foreach(string fileName in fileEntries)
-            ProcessFile(fileName);
-
-        // Recurse into subdirectories of this directory.
-        string [] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
-        foreach(string subdirectory in subdirectoryEntries)
-            ProcessDirectory(subdirectory);
-    }
-    
-    private static void ProcessFile(string path)
-    {
-        // Debug.Log(string.Format("Processed file '{0}'.", path));
-        if (path.Contains("FindTODOTool"))
+namespace WRA.Tools.Editor
+{
+    public class FindTODOTool : MonoBehaviour
+    { 
+        [MenuItem("thief01/Tools/Find TODO's comments")]
+        public static void Find()
         {
-            return;
+            ProcessDirectory(Application.dataPath);
         }
-        
-        var lines = File.ReadLines(path).ToList();
-
-        for (int i = 0; i < lines.Count; i++)
+    
+        private static void ProcessDirectory(string targetDirectory)
         {
-            if (lines[i].Contains("// TODO:"))
+            // Process the list of files found in the directory.
+            string [] fileEntries = Directory.GetFiles(targetDirectory, "*.cs");
+            foreach(string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string [] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach(string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+    
+        private static void ProcessFile(string path)
+        {
+            // Debug.Log(string.Format("Processed file '{0}'.", path));
+            if (path.Contains("FindTODOTool"))
             {
-                LogTodo(path, i.ToString(), lines[i]);
+                return;
+            }
+        
+            var lines = File.ReadLines(path).ToList();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (lines[i].Contains("// TODO:"))
+                {
+                    LogTodo(path, i.ToString(), lines[i]);
+                }
             }
         }
-    }
 
-    private static void LogTodo(string file, string lineNumber, string commentTodo)
-    {
-        WraDiagnostics.Log($"Found TODO in file {Path.GetFileName(file)} in line {lineNumber} comment TODO: {commentTodo}", Color.yellow);
+        private static void LogTodo(string file, string lineNumber, string commentTodo)
+        {
+            WraDiagnostics.Log($"Found TODO in file {Path.GetFileName(file)} in line {lineNumber} comment TODO: {commentTodo}", Color.yellow);
+        }
     }
 }

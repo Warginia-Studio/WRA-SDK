101e23dc9216139dc808f3b15f9e9fe813937ad1
thief01 <pst12345@wp.pl>
2023-09-23 22:54:13 +0200

    move and fix text translator

---
+++ WRA/PlayerSystems/LanguageSystem/TextTranslator.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using WRA.PlayerSystems.LanguageSystem;
+
+namespace WRA.UI.Controls
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TextTranslator : MonoBehaviour
+    {
+        [SerializeField] private string textKey;
+        
+        private TMP_Text tmpText;
+
+        private void Awake()
+        {
+            tmpText = GetComponent<TMP_Text>();
+            RegisterEvents();
+            UpdateLang();
+        }
+
+        private void OnDestroy()
+        {
+            UnRegisterEvents();
+        }
+        
+        private void RegisterEvents()
+        {
+            LanguageManager.LanguageChanged.AddListener(UpdateLang); 
+        }
+
+        private void UnRegisterEvents()
+        {
+            LanguageManager.LanguageChanged.RemoveListener(UpdateLang);
+        }
+
+        public void SetTextKey(string textKey)
+        {
+            this.textKey = textKey;
+        }
+
+        protected virtual void UpdateLang()
+        {
+            tmpText.text = LanguageManager.GetTranslation(textKey);
+        }
+    }
+}

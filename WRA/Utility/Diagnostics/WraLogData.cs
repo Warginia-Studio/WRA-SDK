34fb139e1503d8f718978666eea5daf47a02a36a
thief01 <pst12345@wp.pl>
2023-09-24 00:55:53 +0200

    refactor log sytem

---
+++ WRA/Utility/Diagnostics/WraLogData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WraLogData : MonoBehaviour
+{
+    public WraLogType LogType { get; set; }
+    public string Message { get; set; }
+}
---
+++ WRA/Utility/Diagnostics/WraLogType.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WraLogType 
+{
+    log,
+    error,
+    warning
+}

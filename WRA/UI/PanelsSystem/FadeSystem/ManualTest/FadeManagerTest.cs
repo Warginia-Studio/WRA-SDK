53a5b138ecdd053cf642440efed2b4abaf261f81
thief01 <pst12345@wp.pl>
2023-09-25 06:55:01 +0200

    add fade screen manual tests

---
+++ WRA/UI/PanelsSystem/FadeSystem/ManualTest/FadeManagerTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WRA.UI.PanelsSystem;
+using WRA.UI.PanelsSystem.FadeSystem;
+
+public class FadeManagerTest : MonoBehaviour
+{
+    private FadeManager fadeManager;
+    private void Awake()
+    {
+        fadeManager = PanelManager.Instance.OpenPanel<FadeManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            fadeManager.FadeIn();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            fadeManager.FadeOut();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            fadeManager.FadeInOut();
+        }
+    }
+}

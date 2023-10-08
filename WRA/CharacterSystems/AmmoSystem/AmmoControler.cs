126fe0a292c7759ef4f437aa14b2111061d05544
thief01 <pst12345@wp.pl>
2023-10-09 00:15:04 +0200

    add trajectory helper

--- WRA/CharacterSystems/AmmoSystem/AmmoControler.cs
+++ WRA/CharacterSystems/AmmoSystem/AmmoControler.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AmmoControler : CharacterSystemBase
 {
+    public UnityEvent OnAmmoChange;
+    
     [SerializeField] private List<AmmoData> AmmoDatas;
 
+    public void RemoveAmmo(string ammoName, int count = 1)
+    {
+        var ammodata = AmmoDatas.Find(ctg => ctg.AmmoName == ammoName);
+        if (ammodata == null)
+            return;
+        ammodata.AmmoCont -= count;
+    }
     public int GetAmmoCount(string ammoName)
     {
         return AmmoDatas.Find(ctg => ctg.AmmoName.ToLower() == ammoName.ToLower()).AmmoCont;
---
+++ WRA/Utility/Trajectory2DHelper.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WRA.Utility
+{
+    public class Trajectory2DHelper
+    {
+        private const float TIME_STEP = 1f / 60f;
+        private const float COLLIDER_CHECK = 0.1f;
+        public static Vector3[] CalculateTracjetory(Rigidbody2D rigidbody2D, int frames, int layers)
+        {
+            return CalculaceTrajctory(rigidbody2D, rigidbody2D.velocity, frames, layers);
+        }
+
+        public static Vector3[] CalculaceTrajctory(Rigidbody2D rigidbody2D, Vector2 startVelocity, int frames, int layers)
+        {
+            List<Vector3> points = new List<Vector3>();
+            Vector2 velocity = startVelocity;
+            Vector2 currentPosition = Vector3.zero;
+            points.Add(currentPosition);
+            
+            for (int i = 0; i < frames; i++)
+            {
+                currentPosition += velocity * TIME_STEP;
+                points.Add(currentPosition);    
+                velocity += Physics2D.gravity * TIME_STEP;
+                velocity -= velocity * rigidbody2D.drag;
+                if (Physics2D.OverlapCircle(rigidbody2D.position + currentPosition, COLLIDER_CHECK, layers) != null)
+                {
+                    break;
+                }
+            }
+            
+            return points.ToArray();
+        }
+    }
+}

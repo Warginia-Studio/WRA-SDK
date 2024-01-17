95fb28b10543a4dc1acc507b9fe91d5c80de9108
thief01 <pst12345@wp.pl>
2024-01-17 02:02:47 +0100

    changes list with history

--- WRA/Utility/ListWithHistory.cs
+++ WRA/Utility/ListWithHistory.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
+using Object = UnityEngine.Object;
 
 namespace WRA.Utility
 {
-    public class ListWithHistory<T> where T : new ()
+    public class ListWithHistory<T> where T : Object
     {
         public List<T> Objects { get; private set; }
         public List<T> LastRemovedObjects { get; private set; }
@@ -17,6 +19,12 @@ namespace WRA.Utility
         {
             LastRemovedObjects = Objects.FindAll(ctg => !objects.Contains(ctg));
             Objects = objects;
+        }
+        
+        public void SetObjects(T[] objects)
+        {
+            LastRemovedObjects = Objects.FindAll(ctg => Array.IndexOf(objects, ctg) == -1);
+            Objects = new List<T>(objects);
         }
     }
 }

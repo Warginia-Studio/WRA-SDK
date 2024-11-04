aed5473ec3a069da06893cfc8c0744e704727f6f
thief01 <pst12345@wp.pl>
2024-11-04 22:16:28 +0100

    fix null exception

--- WRA/CharacterSystems/Data/TargetInfo2D.cs
+++ WRA/CharacterSystems/Data/TargetInfo2D.cs
@@ -8,26 +8,27 @@ namespace Character.Bullet
     public class TargetInfo2D : InfoBase
     {
         public LayerMask EnemyLayer { get; set; }
-        
+
         public Transform Owner { get; set; }
 
         public Collider2D ColliderTarget { get; set; }
         private Vector3 TargetPosition { get; set; }
         private bool IsDummyTarget { get; set; }
-        
-        public TargetInfo2D(Transform owner, Collider2D colliderTarget, LayerMask enemyLayer, bool isDummyTarget = false) : base(owner, colliderTarget.transform)
+
+        public TargetInfo2D(Transform owner, Collider2D colliderTarget, LayerMask enemyLayer,
+            bool isDummyTarget = false) : base(owner, colliderTarget != null ? colliderTarget.transform : null)
         {
             Owner = owner;
             ColliderTarget = colliderTarget;
             EnemyLayer = enemyLayer;
             IsDummyTarget = isDummyTarget;
         }
-        
+
         public void SetDummyTarget(Vector3 targetPosition)
         {
             TargetPosition = targetPosition;
         }
-        
+
         public Vector3 GetTargetPosition(Vector3 from)
         {
             return IsTargetReallyNull() ? TargetPosition : ColliderTarget.ClosestPoint(from);
@@ -39,6 +40,7 @@ namespace Character.Bullet
             {
                 return false;
             }
+
             return ColliderTarget == null;
         }
 

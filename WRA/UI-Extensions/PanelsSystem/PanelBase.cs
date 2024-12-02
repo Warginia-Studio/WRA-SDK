66454fa57c851943c3028d76f1e1d45a4e29ddbb
thief01 <pst12345@wp.pl>
2024-12-02 01:36:52 +0100

    fix panel

--- WRA/UI-Extensions/PanelsSystem/PanelBase.cs
+++ WRA/UI-Extensions/PanelsSystem/PanelBase.cs
@@ -88,11 +88,7 @@ namespace WRA.UI.PanelsSystem
         {
             OnCloseEvent?.Invoke();
             
-            if (Animations == null || Animations.Count == 0)
-            {
-                canvasGroup.alpha = 0;
-                return;
-            }
+
 
             Fragments.ForEach(ctg =>
             {
@@ -100,6 +96,11 @@ namespace WRA.UI.PanelsSystem
                     return;
                 ctg.OnClose();
             });
+            
+            if (Animations.Count == 0)
+            {
+                canvasGroup.alpha = 0;
+            }
             
             Animations.ForEach(ctg =>
             {
@@ -119,18 +120,19 @@ namespace WRA.UI.PanelsSystem
             OnShowEvent?.Invoke();
             SetCanvasGroup(true);
             
-            if (Animations == null || Animations.Count == 0)
-            {
-                canvasGroup.alpha = 1;
-                return;
-            }
-            
+
             Fragments.ForEach(ctg =>
             {
                 if (ctg == null)
                     return;
                 ctg.OnShow();
             });
+            
+            if (Animations.Count == 0)
+            {
+                canvasGroup.alpha = 1;
+            }
+
 
             Animations.ForEach(ctg =>
             {
@@ -150,18 +152,18 @@ namespace WRA.UI.PanelsSystem
             OnHideEvent?.Invoke();
             SetCanvasGroup(false);
             
-            if (Animations == null || Animations.Count == 0)
-            {
-                canvasGroup.alpha = 0;
-                return;
-            }
-            
             Fragments.ForEach(ctg =>
             {
                 if (ctg == null)
                     return;
                 ctg.OnHide();
             });
+            
+            if (Animations == null)
+            {
+                canvasGroup.alpha = 0;
+                return;
+            }
             
             Animations.ForEach(ctg =>
             {
